ae23310391f2972056f7eb73caadd831db3cc9d9
Laitzu <saku.tarkiainen@gmail.com>
2025-05-18 14:00:35 +0300

    Working Doubly linked list for storing read lines

--- Project1/reverse.c
+++ Project1/reverse.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Function declarations
-FILE* openFile(char *fileName, char *mode);
-void readLines(FILE *stream);
-
+#include <string.h>
 
 // Doubly linked list from task4 week 9
-struct Lines
+typedef struct Lines
 {
 	char *string;
 	struct Lines *prev;
 	struct Lines *next;
-};
+} Lines;
+
+// Function declarations
+FILE* openFile(char *fileName, char *mode);
+Lines* readLines(FILE *stream, Lines *lineStart);
+Lines* allocateMemory(Lines* node, size_t stringSize);
+void printNodes(Lines *listStart);
+
 
 
 int main(int argc, char *argv[]) {
+
+    FILE *file = NULL;
+    Lines *listStart = NULL;
 
     // Check for too many input arguments
     if(argc > 3) {
@@ -25,14 +31,16 @@ int main(int argc, char *argv[]) {
 
     if(argc == 2) {
         // Open file for reading
-        FILE *file = openFile(argv[1], "r");
+        file = openFile(argv[1], "r");
 
         // Read lines from the file
-        readLines(file);
+        listStart = readLines(file, listStart);
 
         // Close file
         fclose(file);
     }
+
+    printNodes(listStart);
 
     return 0;
 }
@@ -49,14 +57,57 @@ FILE* openFile(char *fileName, char *mode) {
     return file;
 }
 
-void readLines(FILE* stream) {
+Lines* readLines(FILE* stream, Lines* listStart) {
 
     char *string = NULL;
     size_t size = 0;
     __ssize_t chars_read;
+    Lines* lineNode = NULL;
 
+    // Read first line and save it to first node
+    chars_read = getline(&string, &size, stream);
+
+    // Allocate memory for first node depending on line (string) size
+    listStart = allocateMemory(listStart, size);
+    // Store line in the node
+    strcpy(listStart->string, string);
+
+    // Loop through the rest of the lines storing them in the doubly linked list's nodes
     while((chars_read = getline(&string, &size, stream)) > 0) {
-        printf("%ld characters read: %s", chars_read, string);
+        // Allocate memory for the node
+        lineNode = allocateMemory(lineNode, size);
+        // Store string inside of the node
+        strcpy(lineNode->string, string);
+
+        // Set relations for the previous node and current node
+        listStart->next = lineNode;
+        lineNode->prev = listStart;
+        // Set lineStart to be the current node for next iteration
+        listStart = lineNode;
     }
 
+    // listStart is now the last node in the doubly linked list which is convenient for this task
+    return listStart;
+}
+
+Lines* allocateMemory(Lines* node, size_t stringSize) {
+    // Allocate memory for line struct
+    if((node = (Lines *)malloc(sizeof(Lines))) == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    // Allocate memory for string inside this struct
+    if((node->string = (char *)malloc(stringSize)) == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    return node;
+}
+
+void printNodes(Lines *listStart) {
+    // Iterate through the nodes of the list, conveniently listStart is the last node of the list
+    while(listStart->prev != NULL) {
+        printf("%s", listStart->string);
+        listStart = listStart->prev;
+    }
 }
